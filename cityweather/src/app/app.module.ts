40c0fa2aaba78fc7db5af883aade2999fd697b33
kaliloteixeira <kalilo555@hotmail.com>
2023-04-17 00:09:33 -0300

    updating package.json

--- cityweather/src/app/app.module.ts
+++ cityweather/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
@@ -18,7 +19,8 @@ import { AddComponent } from './components/add/add.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- cityweather/src/app/components/citylist/citylist.component.ts
+++ cityweather/src/app/components/citylist/citylist.component.ts
@@ -13,6 +13,13 @@ export class CitylistComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  removeCity(city: City): void {
+    const index = this.cities.findIndex(c => c.id === city.id);
+    if (index !== -1) {
+      this.cities.splice(index, 1);
+    }
+  }
+
   cities: City[] = [
     {
       id: 1,
@@ -31,8 +38,51 @@ export class CitylistComponent implements OnInit {
       cityname: "New York",
       temperature: 283.1,
       timestamp: new Date("2023-04-16T16:27:00.387")
+    },
+    {
+      id: 4,
+      cityname: "Tokyo",
+      temperature: 290.4,
+      timestamp: new Date("2023-04-16T16:27:00.387")
+    },
+    {
+      id: 5,
+      cityname: "London",
+      temperature: 281.9,
+      timestamp: new Date("2023-04-16T16:27:00.387")
+    },
+    {
+      id: 6,
+      cityname: "Sydney",
+      temperature: 295.6,
+      timestamp: new Date("2023-04-16T16:27:00.387")
+    },
+    {
+      id: 7,
+      cityname: "Dubai",
+      temperature: 304.2,
+      timestamp: new Date("2023-04-16T16:27:00.387")
+    },
+    {
+      id: 8,
+      cityname: "Moscow",
+      temperature: 278.6,
+      timestamp: new Date("2023-04-16T16:27:00.387")
+    },
+    {
+      id: 9,
+      cityname: "Rome",
+      temperature: 290.1,
+      timestamp: new Date("2023-04-16T16:27:00.387")
+    },
+    {
+      id: 10,
+      cityname: "Rio de Janeiro",
+      temperature: 295.9,
+      timestamp: new Date("2023-04-16T16:27:00.387")
     }
   ];
+  
   
 
 }
